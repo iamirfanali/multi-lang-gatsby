5cdb180be4903016bab2d68d6eadf51464a4172b
iamirfanali <irfanaliamanat@gmail.com>
2019-09-02 15:04:02 +0500

    gatsby-plugin-i18n added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
+const languages = require("./src/data/languages")
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Multi Language Gatsby Demo`,
+    description: `Gatsby multilanguage demo using gatsby-plugin-i18n`,
+    author: `@iamirfanali`,
+    languages,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +14,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+      resolve: "gatsby-plugin-i18n",
+      options: {
+        langKeyForNull: "any",
+        langKeyDefault: languages.defaultLangKey,
+        useLangKeyLayout: true,
+        prefixDefault: false,
       },
     },
     `gatsby-transformer-sharp`,
